9595b3b3bca5aca60be856d0e9b2cb16388765af
Arkoski <arkoski93@gmail.com>
2018-07-10 18:18:06 +0200

    Feature/email activation sending (#16)

---
+++ src/main/java/com/github/project/controller/RegistrationConfirmController.java
@@ -0,0 +1,25 @@
+package com.github.project.controller;
+
+import com.github.project.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RegistrationConfirmController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/registrationConfirm")
+    public String loginError(@RequestParam("token")String token) {
+
+        clientService.activateUser(token);
+//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//        model.addAttribute("error", true);
+
+        return "registrationConfirm";
+    }
+
+}
